b09362c0ef28f24caf29404b2c66d9fa3d2953c9
valdeirxboxtec <xboxtec@hotmail.com>
2021-04-05 22:30:10 -0300

    Create aula.c
    aula de linguagem c

---
+++ aula.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main()
+{
+
+    int nro;
+    float f;
+
+    printf("Digite dois numeros : \n");
+    scanf("%d %f", &nro,&f);
+    printf("Numeros: %d %f\n", nro, f);
+
+    system("pause");
+    return 0;
+}
